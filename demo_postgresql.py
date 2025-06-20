0dd60d9a53abf85a998787fc2545acb6f4e6c456
Kevin Wu <kevin5269@gmail.com>
2025-06-20 15:36:19 +0800

    First Commit

---
+++ demo_postgresql.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    "postgresql://admin:123456@127.0.0.1:5432/testdb"
+
+db = SQLAlchemy(app)
+
+class Students(db.Model):
+
+    __tablename__ = "Students"
+    sid  = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    tel  = db.Column(db.String(50))
+    addr = db.Column(db.String(200))
+    email= db.Column(db.String(100))
+
+    def __init__(self, name, tel, addr, email):
+        self.name = name
+        self.tel = tel
+        self.addr = addr
+        self.email= email
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
