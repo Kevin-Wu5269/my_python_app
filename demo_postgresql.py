60896b47da394af07c8a492c7284ce06ce69fc51
Kevin Wu <kevin5269@gmail.com>
2025-06-20 16:24:01 +0800

    update to v2

--- demo_postgresql.py
+++ demo_postgresql.py
@@ -22,8 +22,29 @@ class Students(db.Model):
         self.addr = addr
         self.email= email
 
+@app.route("/")
+def index():
+    db.create_all()
+    return "資料庫連線成功"
 
+@app.route("/insert")
+def insert():
+    student = Students("Apple","0911111111","新北市中和區中和路5號","apple@gmail.com")
+    db.session.add(student)
+    db.session.commit()
+    return "新增一筆紀錄成功"
 
+@app.route("/insertAll")
+def insertAll():
+    student1 = Students("Bill","0922222222","新北市中和區永和路5號","bill@gmail.com")
+    student2 = Students("Cathy","0933333333","台北市中山區中山北路一段5號","cathy@gmail.com")
+    student = (student1,student2)
+    db.session.add_all(student)
+    db.session.commit()
+    return "新增多筆紀錄成功"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
 
 
 
